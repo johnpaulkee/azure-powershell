904c8aa2238ca294407984fceb30935c588b3795
John Paul Kee <joke@microsoft.com>
2019-06-25 10:44:39 -0700

    Init instance pool cmdlets

---
+++ src/Sql/Sql.Test/ScenarioTests/InstancePoolCrudTests.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.Azure.Commands.ScenarioTest.SqlTests;
+using Microsoft.WindowsAzure.Commands.ScenarioTest;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace Microsoft.Azure.Commands.Sql.Test.ScenarioTests
+{
+    public class InstancePoolCrudTests : SqlTestsBase
+    {
+        public InstancePoolCrudTests(ITestOutputHelper output) : base(output)
+        {
+        }
+
+        #region Create Tests
+
+        [Fact]
+        [Trait(Category.AcceptanceType, Category.CheckIn)]
+        public void TestInstancePoolCreate()
+        {
+            RunPowerShellTest("Test-CreateInstancePool");
+        }
+
+        #endregion
+
+        #region Update Tests
+
+        [Fact]
+        [Trait(Category.AcceptanceType, Category.CheckIn)]
+        public void TestInstancePoolUpdate()
+        {
+            RunPowerShellTest("Test-UpdateInstancePool");
+        }
+
+        #endregion
+
+        #region Get Tests
+
+        [Fact]
+        [Trait(Category.AcceptanceType, Category.CheckIn)]
+        public void TestInstancePoolRead()
+        {
+            RunPowerShellTest("Test-GetInstancePool");
+        }
+
+        #endregion
+
+        #region Remove Tests
+
+        [Fact]
+        [Trait(Category.AcceptanceType, Category.CheckIn)]
+        public void TestInstancePoolRemove()
+        {
+            RunPowerShellTest("Test-RemoveInstancePool");
+        }
+
+        #endregion
+    }
+}
---
+++ src/Sql/Sql/Instance Pools/Cmdlet/InstancePoolCmdletBase.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.Azure.Commands.Common.Authentication.Abstractions;
+using Microsoft.Azure.Commands.Sql.Common;
+using Microsoft.Azure.Commands.Sql.Instance_Pools.Model;
+using Microsoft.Azure.Commands.Sql.Instance_Pools.Services;
+
+namespace Microsoft.Azure.Commands.Sql.Instance_Pools.Cmdlet
+{
+    public abstract class InstancePoolCmdletBase : AzureSqlCmdletBase<IEnumerable<AzureSqlInstancePoolModel>, AzureSqlInstancePoolAdapter>
+    {
+        /// <summary>
+        /// Initializes the instance pool model adapter.
+        /// </summary>
+        /// <param name="subscription">The subscription the cmdlets are in operation with</param>
+        /// <returns>The instance pool adapter</returns>
+        protected override AzureSqlInstancePoolAdapter InitModelAdapter(IAzureSubscription subscription)
+        {
+            return new AzureSqlInstancePoolAdapter(DefaultContext);
+        }
+    }
+}
